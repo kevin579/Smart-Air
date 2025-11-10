186eb17713208b1bda365c20f13e1b5a365039e9
randyyang <randyy@seycove.ca>
2025-11-09 19:29:07 -0500

    feat: added UI for login page, new user page, app logo fix: added fragment container for activity_home_fragment.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
@@ -36,6 +39,8 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/SmartAirGroup2/create_account.java
+++ app/src/main/java/com/example/SmartAirGroup2/create_account.java
@@ -7,54 +7,58 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
-
+public class create_account extends AppCompatActivity {
     private EditText username;
     private EditText password;
-    private Button loginButton;
-
     private Button createButton;
-
     private FirebaseDatabase db;
+
+    public boolean notInDatabase(String userInput){
+        return true;
+    }
+
+    public boolean isValid(String passInput){
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_account);
 
         username = findViewById(R.id.username_input);
         password = findViewById(R.id.password_input);
-        loginButton = findViewById(R.id.login_button);
         createButton = findViewById(R.id.new_account_button);
         db = FirebaseDatabase.getInstance("https://smart-air-group2-default-rtdb.firebaseio.com/");
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String userInput = username.getText().toString().trim();
                 String passInput = password.getText().toString().trim();
 
-                if (userInput.equals("user") && passInput.equals("1234")) {
-                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(MainActivity.this, create_account.class);
+                if (notInDatabase(userInput) && isValid(passInput)) {
+                    Toast.makeText(create_account.this, "account registered!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(create_account.this, MainActivity.class);
                     startActivity(intent);
                 }
-                else {
-                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                else if (!notInDatabase(userInput)){
+                    Toast.makeText(create_account.this, "account already exist", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
-
-        createButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, create_account.class);
-                startActivity(intent);
+                else{
+                    Toast.makeText(create_account.this, "Password must be at least 8 characters with uppercase, lowercase, number, and special character", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
+
+
 }
