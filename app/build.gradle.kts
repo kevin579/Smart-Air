718bb93d05c966d893c227d8b95d32163ef32b04
dogepandaCodes <randyy@seycove.ca>
2025-11-18 02:11:24 -0500

    feature: complete UI for badge and win steak, stored in steak_format.xml.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
