6fe016ee3ead530778469e025f9611123b55e704
Perryyyyyyyy <qiu13328224715@outlook.com>
2025-11-25 19:43:31 -0500

    <ImageButton

---
+++ app/src/main/java/com/example/SmartAirGroup2/BadgeLibrary.java
@@ -0,0 +1,32 @@
+package com.example.SmartAirGroup2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BadgeLibrary extends Fragment {
+    private ImageButton btnBack;
+    public BadgeLibrary() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.streak_format, container, false);
+        btnBack = view.findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(v -> requireActivity()
+                .getSupportFragmentManager()
+                .popBackStack()
+        );
+
+        return view;
+    }
+}
